7f85b3daa6e98d73fce5de3b048f88436d8456cc
weezart <weezart@yandex.ru>
2022-07-13 20:58:39 +0300

    3.1 Добавит маршрутизацию.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import MoviePageScreen from '../../pages/movie-page-screen/movie-page-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
 
 const promoFilm = {
   name: 'The Grand Budapest Hotel',
@@ -10,7 +18,38 @@ function App(): JSX.Element {
   const { name, genre, release } = promoFilm;
 
   return (
-    <MainScreen name={name} genre={genre} release={release} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen name={name} genre={genre} release={release} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<SignInScreen />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePageScreen />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListScreen />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,40 @@
+import {Link} from 'react-router-dom';
+
+function NotFoundScreen(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <Link to="/" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </Link>
+        </div>
+
+        <h1 className="page-title user-page__title">Error</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <h1>404. Page not found</h1>
+        <Link to="/">Вернуться на главную</Link>
+      </div>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <Link to="/" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </Link>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
