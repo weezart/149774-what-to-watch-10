76e0e4b7237177e0db5068f4c1ad0a2312e54f67
weezart <weezart@yandex.ru>
2022-07-16 16:31:59 +0300

    4.1 Добавит моки для карточек фильмов.

---
+++ project/src/mocks/films.ts
@@ -0,0 +1,44 @@
+import {Film} from '../types/film';
+
+export const films: Film[] = [
+  {
+    id: 'film1',
+    name: 'Aviator',
+    image: 'img/aviator.jpg'
+  },
+  {
+    id: 'film2',
+    name: 'Bohemian Rhapsody',
+    image: 'img/bohemian-rhapsody.jpg'
+  },
+  {
+    id: 'film3',
+    name: 'Dardjeeling Limited',
+    image: 'img/dardjeeling-limited.jpg'
+  },
+  {
+    id: 'film4',
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+    image: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg'
+  },
+  {
+    id: 'film5',
+    name: 'Johnny English',
+    image: 'img/johnny-english.jpg'
+  },
+  {
+    id: 'film6',
+    name: 'Shutter Island',
+    image: 'img/shutter-island.jpg'
+  },
+  {
+    id: 'film7',
+    name: 'Macbeth',
+    image: 'img/macbeth.jpg'
+  },
+  {
+    id: 'film8',
+    name: 'Mindhunter',
+    image: 'img/mindhunter.jpg'
+  },
+];
---
+++ project/src/types/film.ts
@@ -0,0 +1,14 @@
+export type Film = {
+  id: string;
+  name: string;
+  image: string;
+};
+
+export type FilmInfo = {
+  id: string;
+  poster: string;
+  imageBg: string;
+  name: string;
+  genre: string,
+  release: number,
+};
