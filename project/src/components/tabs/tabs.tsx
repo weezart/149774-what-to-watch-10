008a169759161aa01c45cc2903120de9ddc0fcf8
weezart <weezart@yandex.ru>
2022-07-21 20:19:49 +0300

    5.5 Добавлен обработчик на табы.

--- project/src/components/tabs/tabs.tsx
+++ project/src/components/tabs/tabs.tsx
@@ -1,23 +1,34 @@
 import { FilmInfo } from '../../types/film';
-import React from 'react';
+import React, {useState} from 'react';
 
 type TabsProps = {
   film: FilmInfo;
 };
 
 function Tabs({film} : TabsProps): JSX.Element {
+  const [activeTab, setActiveTab] = useState('Overview');
+
+  const onTabClickHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if (e.currentTarget.textContent !== null) {
+      setActiveTab(e.currentTarget.textContent);
+    }
+  };
+
+
   return (
     <React.Fragment>
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          <li className="film-nav__item film-nav__item--active">
-            <a href="/" className="film-nav__link">Overview</a>
+          <li className={activeTab === 'Overview' ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}>
+            <a href="/" className="film-nav__link" onClick={onTabClickHandler}>Overview</a>
           </li>
-          <li className="film-nav__item">
-            <a href="/" className="film-nav__link">Details</a>
+          <li className={activeTab === 'Details' ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}>
+            <a href="/" className="film-nav__link" onClick={onTabClickHandler}>Details</a>
           </li>
-          <li className="film-nav__item">
-            <a href="/" className="film-nav__link">Reviews</a>
+          <li className={activeTab === 'Reviews' ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}>
+            <a href="/" className="film-nav__link" onClick={onTabClickHandler}>Reviews</a>
           </li>
         </ul>
       </nav>
