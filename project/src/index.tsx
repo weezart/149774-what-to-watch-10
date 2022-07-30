3bd02acf71e5f7e907d52a2d0ab17f3b73bdf438
weezart <weezart@yandex.ru>
2022-07-30 18:08:25 +0300

    7.12. Добавит обработку ошибок взаимодействия с сервером.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,6 +5,9 @@ import App from './components/app/app';
 import {promoFilm, films, filmsInfo} from './mocks/films';
 import ErrorMessage from './components/error-message/error-message';
 import {store} from './store';
+import {checkAuthAction} from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
