62be33adde11ec4ef7f203c496eec0c0e93e30a9
weezart <weezart@yandex.ru>
2022-08-12 18:47:50 +0300

    7.36 Поправлено получение данных на странице добавления отзыва.

--- project/src/pages/add-review-screen/add-review-screen.tsx
+++ project/src/pages/add-review-screen/add-review-screen.tsx
@@ -1,19 +1,25 @@
 import AddReviewForm from '../../components/add-review-form/add-review-form';
 import Header from '../../components/header/header';
 import {useParams} from 'react-router-dom';
-import {useAppSelector} from '../../hooks';
+import {useAppDispatch, useAppSelector} from '../../hooks';
+import {useEffect} from 'react';
+import {fetchFilmAction} from '../../store/api-actions';
 
 function AddReviewScreen(): JSX.Element {
+  const dispatch = useAppDispatch();
   const params = useParams();
-  const id = `${(params.id ? params.id.slice(1) : '0')}`;
-  const films = useAppSelector((state) => state.films);
-  const film = films.find((item) => item.id === Number.parseInt(id, 10)) || films[0];
+  const film = useAppSelector((state) => state.film);
+
+  useEffect(() => {
+    const id = `${(params.id ? params.id.slice(1) : '0')}`;
+    dispatch(fetchFilmAction(id));
+  }, [params?.id, dispatch]);
 
   return (
     <section className="film-card film-card--full">
       <div className="film-card__header">
         <div className="film-card__bg">
-          <img src={film.backgroundImage} alt={film.name}/>
+          <img src={film?.backgroundImage} alt={film?.name}/>
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -21,7 +27,7 @@ function AddReviewScreen(): JSX.Element {
         <Header />
 
         <div className="film-card__poster film-card__poster--small">
-          <img src={film.posterImage} alt={film.name && ' poster'} width="218"
+          <img src={film?.posterImage} alt={film?.name && ' poster'} width="218"
             height="327"
           />
         </div>
